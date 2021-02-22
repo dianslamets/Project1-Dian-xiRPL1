86f406058f953ab4e8f9c327dc53efae5cfec675
dianslamets <dianslametsaputra@gmail.com>
2021-02-22 09:35:01 +0700

    Aplikasi Kasir

---
+++ src/main/MainAplikasiKasir.java
@@ -0,0 +1,9 @@
+package main;
+/*
+    @author: Dian Slamet Saputra | XI RPL 1
+ */
+public class MainAplikasiKasir {
+    public static void main(String[] args) { }
+    
+    public void generateDaftarMenu() { }
+}
