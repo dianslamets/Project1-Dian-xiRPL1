992fe63a94f68261bfee7ac6f4712b1fc7573196
dianslametsaputra <dianslametsaputra@gmail.com>
2021-06-10 17:57:13 +0700

    Modul D: Mencetak Struk

--- src/classes/Pesanan.java
+++ src/classes/Pesanan.java
@@ -7,9 +7,20 @@ public class Pesanan {
     private int jumlah;
     private String keterangan;
     
-    public Pesanan (Menu menu, int jumlah) { }
+    public Pesanan (Menu menu, int jumlah) {
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
     
     public void setKeterangan (String keterangan) { }
     
     public String getKeterangan() { return ""; }
+    
+    public Menu getMenu() {
+        return menu;
+    }
+    
+    public int getJumlah() {
+        return jumlah;
+    }
 }
--- src/classes/Transaksi.java
+++ src/classes/Transaksi.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
     @author: Dian Slamet Saputra | XI RPL 1
  */
 public class Transaksi {
-    private String noTranksaksi;
+    private String noTransaksi;
     private String namaPemesan;
     private String tanggal;
     private String noMeja;
@@ -15,9 +15,10 @@ public class Transaksi {
     private double pajak;
     private double totalBayar;
     private double biayaService=0;
+    private double service;
     
     public Transaksi (String no_transaksi, String nm_pemesan, String tanggal, String no_meja) {
-        this.noTranksaksi = no_transaksi;
+        this.noTransaksi = no_transaksi;
         this.namaPemesan = nm_pemesan;
         this.tanggal = tanggal;
         this.noMeja = no_meja;
@@ -33,9 +34,67 @@ public class Transaksi {
         return pesanan;
     }
     
-    public double hitungTotalBayar() { return 0; }
+    public void setBiayaService(double service) {
+        this.biayaService = service;
+    }
+    public void setPajak(double pajak) {
+        this.pajak = pajak;
+    }
     
-    public double hitungKembalian() { return 0; }
+    public double hitungTotalPesanan() {
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan psn = pesanan.get(i);
+            double harga = psn.getMenu().getHarga();
+            totalBayar += (harga * psn.getJumlah());
+        }
+        return totalBayar;
+    }
     
-    public void cetakStruk() { }
+    public double hitungPajak() {
+        return totalBayar * pajak;
+    }
+    
+    public double hitungBiayaService() {
+        return totalBayar * biayaService;
+    }
+    
+    public double hitungTotalBayar() {
+        totalBayar = totalBayar + pajak + service;
+        return totalBayar;
+    }
+    
+    public double hitungKembalian(double uang_bayar) {
+        return uang_bayar - totalBayar; // bisa dibuat validator?
+    }
+    
+    public void cetakStruk() {
+        System.out.println("\n========== ALDEBARAMEN ==========");
+        System.out.println("No Transaksi : "+noTransaksi);
+        System.out.println("Pemesan : "+namaPemesan);
+        System.out.println("Tanggal : "+tanggal);
+        
+        //cek jika nomor meja kosong, berarti take away
+        if(noMeja.equals("")) {
+            noMeja = "Take Away";
+        }
+        
+        System.out.println("Meja : "+noMeja);
+        System.out.println("=========================");
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan psn = pesanan.get(i);
+            Menu m = psn.getMenu();
+            String pesanan = psn.getJumlah() + " " + m.getNama_menu() + "\t" + (m.getHarga()*psn.getJumlah());
+            
+            //jika pesanan kuah, tambah spasi di awal 2
+            if (m.getKategori().equals("Kuah")) {
+                pesanan = " "+pesanan;
+            }
+            //tampilkan pesanan
+            System.out.println(pesanan);
+        }
+    }
+
+    public String hitungTotalBayar(double ppn, double biaya_service) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
